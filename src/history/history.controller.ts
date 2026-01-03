19822bcb82f37753bfd1aa2e88cbe4b53979006e
NguyenPhuTrang <nguyenphutrang05@gmail.com>
2026-01-03 12:02:23 +0700

    update history controller

--- src/history/history.controller.ts
+++ src/history/history.controller.ts
@@ -7,28 +7,24 @@ import { HistoryQueryDto } from './dto/history-query.dto';
 @Controller('history')
 @UseGuards(JwtAuthGuard)
 export class HistoryController {
-  constructor(private readonly historyService: HistoryService) {}
+  constructor(private readonly historyService: HistoryService) { }
 
   @Get()
   async list(@Query() query: HistoryQueryDto, @Req() req: any) {
     const role = (req.user?.role || 'STAFF') as 'ADMIN' | 'STAFF';
-
-    // ✅ lấy userId robust (tùy bạn đang set token thế nào)
     const userIdRaw = req.user?.sub || req.user?.id || req.user?._id;
 
     if (role === 'STAFF') {
       if (!userIdRaw || !Types.ObjectId.isValid(String(userIdRaw))) {
-        // nếu token không có id đúng -> sẽ không lọc được
         throw new BadRequestException('Invalid user id in token (req.user.sub)');
       }
     }
-
     return this.historyService.queryLogs({
       query,
       role,
       userId: userIdRaw ? String(userIdRaw) : '',
-      page: query.page ?? 1,
-      limit: query.limit ?? 50,
+      page: query.page ? Number(query.page) : 1,
+      limit: query.limit ? Number(query.limit) : 1000,
     });
   }
 }
